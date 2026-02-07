6e201e53975409326647e661b3e48e927cb0a450
Emad <abbasianemad5@gmail.com>
2026-02-07 04:59:07 -0800

    Initialize admin Blueprint

---
+++ mod_admin/__init__.py
@@ -0,0 +1,13 @@
+from flask import Blueprint
+
+admin = Blueprint('admin', __name__, url_prefix = '/admin/')
+
+
+
+@admin.route('/')
+def admin_index():
+    return 'Hello from admin index,'
+
+
+
+
