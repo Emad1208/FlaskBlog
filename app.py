e3b8cb2edf850409a454ad8b04971ed351df4886
Emad <abbasianemad5@gmail.com>
2026-02-11 02:52:17 -0800

    add view file to the app file

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from config import Development
+from config import Development, Production
 from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Development)
-
+from views import index
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-@app.route('/')
-def index():
-    return 'Home Blog ,'
+
 
 from mod_admin import admin
 from mod_users import users
